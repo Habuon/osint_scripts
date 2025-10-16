ebfd145a4f468a8c5ee9e94534fe8d405959cf20
Habuon <59359193+Habuon@users.noreply.github.com>
2025-10-16 08:32:02 -0400

    Enhance resolver.py with command-line arguments
    Refactor resolver.py to accept additional hosts and IPs as command-line arguments. Update find_domains function to include additional hosts in domain discovery.

--- resolver.py
+++ resolver.py
@@ -3,6 +3,7 @@ import random
 import json
 from string import ascii_lowercase
 from subprocess import PIPE, STDOUT, Popen
+import argparse
 
 
 def reverse_resolve(ips: list) -> dict:
@@ -28,9 +29,9 @@ def resolve_ips(domains: list) -> dict:
 	return servers
 
 
-def find_domains(base: str) -> list:
+def find_domains(base: str, additional_hosts: list) -> list:
 	if os.path.isfile(".domains"):
-		result = []
+		result = additional_hosts
 		with open(".domains", "r") as f:
 			for line in f:
 				line = line.strip()
@@ -42,7 +43,7 @@ def find_domains(base: str) -> list:
 	command = f"subfinder -d {base} -silent"
 	with Popen(command, stdout=PIPE, stderr=STDOUT, shell=True) as process:
 			output = process.communicate()[0].decode("utf-8").strip().split("\n")
-	output = [x for x in output if len(x) != 0]
+	output = list(set([x for x in output if len(x) != 0] + additional_hosts))
 	with open(".domains", "w") as f:
 		for line in output:
 			print(line, file=f)
@@ -70,7 +71,9 @@ def identify_dns_wildcard(base):
 		command = f"dig +short a {random_sub}.{base}"
 		with Popen(command, stdout=PIPE, stderr=STDOUT, shell=True) as process:
 			output = process.communicate()[0].decode("utf-8").strip().split("\n")
-		output = output[-1]
+		output = output[-1].strip()
+		if len(output) == 0:
+			continue
 		possible_wildcards[output] = possible_wildcards.get(output, 0) + 1
 	return possible_wildcards
 
@@ -82,25 +85,38 @@ def print_heading(text: str, line_width: int) -> None:
 	print("-" * line_width)
 
 
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-b", "--base-host", type=str, help="Base host for which the script will do its business", required=True)
+	parser.add_argument("--additional-ips", type=str, help="Additional ips that are part of scope and are obtained by other means separated by comma (e.x. 127.0.0.1,0.0.0.0)", required=False)
+	parser.add_argument("--additional-hosts", type=str, help="Additional hosts that are part of scope and are obtained by other means separated by comma (e.x. localhost,loopback)", required=False)
+	args = parser.parse_args()
+	return args
+
 	
 def main():
 	line_width = 200
-	base = "milkagro.sk"
-	additional_ips = ["65.21.124.52", "5.178.48.39", "212.5.207.122", "195.80.166.170"]
+
+	# Args
+	args = parse_args()
+	base = args.base_host
+	additional_ips = [] if args.additional_ips is None else args.additional_ips.split(",")
+	additional_hosts = [] if args.additional_hosts is None else args.additional_hosts.split(",")
 
 	# Find all subdomains by passive means
 	print(f"[*] Running subfinder ... ", end="", flush=True)
-	domains = find_domains(base)
+	domains = find_domains(base, additional_hosts=additional_hosts)
 	print(f"found {len(domains)} domains")
 
 	# Determine possible DNS wildcards
 	possible_wildcards = identify_dns_wildcard(base)
 	sorted_wildcards = sorted(list(possible_wildcards.keys()), key=lambda x: possible_wildcards[x], reverse=True)
 
-	print_heading("Possible DNS Wildcards", line_width)
-	for possible_wildcard in sorted_wildcards:
-		print(f"[*] Server with IP {possible_wildcard} is possible DNS wildcard (tested with 10 randomly generated subdomains and got {possible_wildcards[possible_wildcard]}/10)")
-	print_heading("Possible DNS Wildcards", line_width)
+	if len(sorted_wildcards) != 0:
+		print_heading("Possible DNS Wildcards", line_width)
+		for possible_wildcard in sorted_wildcards:
+			print(f"[*] Server with IP {possible_wildcard} is possible DNS wildcard (tested with 10 randomly generated subdomains and got {possible_wildcards[possible_wildcard]}/10)")
+		print_heading("Possible DNS Wildcards", line_width)
 	
 	# Resolve all IPs of found domains
 	servers = resolve_ips(domains)
@@ -111,22 +127,17 @@ def main():
 	reverse = reverse_resolve(all_ips)
 
 	orgs = dict()
-	print_heading("Running WHOIS", line_width)
-	for ip in all_ips:
-		org = obtain_org_name(ip)
-		orgs[org] = orgs.get(org, []) + [ip]
-	print(json.dumps(orgs, indent=4), flush=True)
-	print_heading("Running WHOIS", line_width)
+	if len(all_ips) != 0:
+		print_heading("Running WHOIS", line_width)
+		for ip in all_ips:
+			org = obtain_org_name(ip)
+			orgs[org] = orgs.get(org, []) + [ip]
+		print(json.dumps(orgs, indent=4), flush=True)
+		print_heading("Running WHOIS", line_width)
 
 	result = {ip: list(set([x for x in servers.get(ip, []) + reverse.get(ip, []) if len(x) != 0])) for ip in all_ips}
 	print(json.dumps(result, indent=4), flush=True)
 	
-	with open("ips", "w") as f:
-		for k in servers.keys():
-			print(k, file=f)
-			
-		
-
 
 if __name__ == "__main__":
 	main()
